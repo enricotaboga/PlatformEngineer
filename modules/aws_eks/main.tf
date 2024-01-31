55e82cb40b2e88390dcbb20c00c5b9bc10a06990
Enrico Taboga <enrico.taboga@matera.com>
2024-01-31 09:02:51 -0300

    Adding eks module

---
+++ modules/aws_eks/main.tf
@@ -0,0 +1,88 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = var.eks_cluster_version
+
+  cluster_endpoint_public_access  = var.eks_cluster_endpoint_public_access
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = var.eks_vpc_id
+  subnet_ids               = var.eks_vpc_subnet_ids
+  control_plane_subnet_ids = var.eks_vpc_control_plane_subnet_ids
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = var.eks_instance_types
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = var.eks_ng_min_size
+      max_size     = var.eks_ng_max_size
+      desired_size = var.eks_ng_desired_size
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "777777777777",
+    "888888888888",
+  ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
